e77608a98b1ff63f01ad8bc309aad5bef6031be0
marine bijon <marine.bijon@edu.itescia.fr>
2024-06-14 20:11:25 +0200

    initial commit
    initial commit

---
+++ 1_Intro/1_Intro/1_Intro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1_Intro
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Test();
+            showText("Show Text");
+        }
+
+        static void Test() {
+            Console.WriteLine("Test");
+        }
+
+         static void showText(string txt) {
+            Console.WriteLine(txt);
+        }
+    }
+}
