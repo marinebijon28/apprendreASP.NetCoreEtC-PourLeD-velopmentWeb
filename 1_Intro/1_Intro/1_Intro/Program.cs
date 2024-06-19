dc9d6139e80a1b77f32fab42f00d9c53552a0e6a
marine bijon <marine.bijon@edu.itescia.fr>
2024-06-19 11:56:42 +0200

    TD AventureTexte console

--- 1_Intro/1_Intro/1_Intro/Program.cs
+++ 1_Intro/1_Intro/1_Intro/Program.cs
@@ -4,19 +4,37 @@ namespace _1_Intro
 {
     class Program
     {
+        // first video
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Test();
             showText("Show Text");
+            readTextConsole();
         }
 
         static void Test() {
             Console.WriteLine("Test");
         }
 
-         static void showText(string txt) {
+        static void showText(string txt) {
             Console.WriteLine(txt);
+        }
+
+        // second video
+        static int age;
+        static void readTextConsole() {
+            Console.WriteLine("Quel est votre age ?");
+            // age = int.Parse(Console.ReadLine());
+            // age = int.TryParse(Console.ReadLine());
+            var res = int.TryParse(Console.ReadLine(), out age);
+            if (res == true)
+                if (age >= 18)
+                    Console.WriteLine("Vous êtes majeurs");
+                else
+                    Console.WriteLine("Vous êtes mineurs");
+            else 
+                Console.WriteLine("Erreur");
         }
     }
 }
---
+++ TD_AventureTexte/TD_AventureTexte/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TD_AventureTexte {
+    /*
+        1. Le joueur est dans une forêt. Il peut aller à l'est ou à l'ouest.
+            1.1. Si est = Il tombe d'une failaise et perd
+            1.2. Si ouest = Il trouve un château 
+        2. Le joueur est devant le château. Il peut entrer par la porte ou par la fenêtre.
+            2.1. Si porte = Le garde l'arrête, il perd
+            2.2. Si fenêtre il entre
+        3. Le joeur est dans un couloir du château, il a la porte de gauche de droite
+            3.1. Si gauche = il tombe sur les gardes et perd
+            3.2. Si droite, il trouve la princesse et la sauve
+
+        Aller plus loin :
+        - Ajouter un inventaire avec une clé ou des objets 
+        - Ajouter plus de chemins  
+        - Ajouter un système de score, vie, magie...
+    */
+
+    class Program {
+        static string readLine = null;
+        static void Main(string[] args) {
+
+            while (readLine == null || (readLine.ToLower() != "est" && readLine.ToLower() != "ouest")) {
+                Console.WriteLine("Vous êtes dans une forêt. Vous pouvez aller à l'est ou à l'ouest.");
+                if ((readLine = Console.ReadLine()) != null) {
+                    if (readLine.ToLower() == "est") {
+                        Console.WriteLine("Vous tombez d'une failaise et perd");
+                        Environment.Exit(0);
+                    }
+                    else if (readLine.ToLower() == "ouest")
+                        Console.WriteLine("Vous trouvez un château");
+                }
+            }
+            readLine = null;
+            
+            while (readLine == null || (readLine.ToLower() != "porte" && readLine.ToLower() != "fenêtre")) {
+                Console.WriteLine("Vous êtes devant le château. Vous pouvez entrer par la porte ou par la fenêtre.");
+                if ((readLine = Console.ReadLine()) != null) {
+                    if (readLine.ToLower() == "porte") {
+                        Console.WriteLine("Le garde vous arrête, vous perdez");
+                        Environment.Exit(0);
+                    }
+                    else if (readLine.ToLower() == "fenêtre")
+                        Console.WriteLine("Vous entrez");
+                }
+            }
+            readLine = null;
+
+            while (readLine == null || (readLine.ToLower() != "gauche" && readLine.ToLower() != "droite")) {
+                Console.WriteLine("Vous entrez dans un couloir du château, vous avez une porte à gauche et à droite");
+                if ((readLine = Console.ReadLine()) != null) {
+                    if (readLine.ToLower() == "gauche") {
+                        Console.WriteLine("Vous tombez sur les gardes et vous perdez");
+                        Environment.Exit(0);
+                    }
+                    else if (readLine.ToLower() == "droite")
+                        Console.WriteLine("Vous trouvez la princesse et vous la sauvez");
+                }
+            }
+        }
+    }
+}
